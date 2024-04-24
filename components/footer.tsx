d35cd21e82a0152ab861ca0ee0fc44be0ed95bd8
aditya-gaharawar <164782232+aditya-gaharawar@users.noreply.github.com>
2024-04-25 01:24:37 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,15 +3,29 @@ import React from 'react'
 import { Button } from './ui/button'
 
 const Footer: React.FC = () => {
-
   return (
     <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0">
       <div className="flex justify-end">
         {/* GitHub, Twitter, and Discord icons removed */}
       </div>
-      <p className="text-sm text-gray-500">WEBSPACEAI Research can make mistakes. Consider checking important information.</p>
     </footer>
   )
 }
 
-export default Footer
+const Disclaimer: React.FC = () => {
+  return (
+    <div className="fixed inset-0 flex justify-center items-center">
+      <p className="text-sm text-gray-500">WEBSPACEAI Research can make mistakes. Consider checking important information.</p>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <div>
+      <Footer />
+      <Disclaimer />
+      {/* Rest of your app content */}
+    </div>
+  )
+}
