2dbeafd47c90dc452038d6c0a8033564e838b31b
aditya-gaharawar <164782232+aditya-gaharawar@users.noreply.github.com>
2024-04-25 01:10:13 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,40 +1,15 @@
 import React from 'react'
-import Link from 'next/link'
-import { SiDiscord, SiGithub, SiTwitter } from 'react-icons/si'
+
 import { Button } from './ui/button'
 
 const Footer: React.FC = () => {
+
   return (
     <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0">
       <div className="flex justify-end">
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://discord.gg/zRxaseCuGq" target="_blank">
-            <SiDiscord size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://twitter.com/miiura" target="_blank">
-            <SiTwitter size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://git.new/morphic" target="_blank">
-            <SiGithub size={18} />
-          </Link>
-        </Button>
+        {/* GitHub, Twitter, and Discord icons removed */}
       </div>
+      <p className="text-sm text-gray-500">WEBSPACEAI Research can make mistakes. Consider checking important information.</p>
     </footer>
   )
 }
