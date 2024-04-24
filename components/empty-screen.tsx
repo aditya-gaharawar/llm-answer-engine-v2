b7579b399febe54edbf04e5b51709094acc81ac0
aditya-gaharawar <164782232+aditya-gaharawar@users.noreply.github.com>
2024-04-25 01:13:40 +0530

    Update empty-screen.tsx

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -1,24 +1,6 @@
 import { Button } from '@/components/ui/button'
 import { ArrowRight } from 'lucide-react'
 
-const exampleMessages = [
-  {
-    heading: 'Why is Nvidia growing rapidly?',
-    message: 'Why is Nvidia growing rapidly?'
-  },
-  {
-    heading: 'Is the Apple Vision Pro worth buying?',
-    message: 'Is the Apple Vision Pro worth buying?'
-  },
-  {
-    heading: 'How does the Vercel AI SDK work?',
-    message: 'How does the Vercel AI SDK work?'
-  },
-  {
-    heading: 'Tesla vs Rivian',
-    message: 'Tesla vs Rivian'
-  }
-]
 export function EmptyScreen({
   submitMessage,
   className
@@ -30,20 +12,7 @@ export function EmptyScreen({
     <div className={`mx-auto w-full transition-all ${className}`}>
       <div className="bg-background p-2">
         <div className="mt-4 flex flex-col items-start space-y-2 mb-4">
-          {exampleMessages.map((message, index) => (
-            <Button
-              key={index}
-              variant="link"
-              className="h-auto p-0 text-base"
-              name={message.message}
-              onClick={async () => {
-                submitMessage(message.message)
-              }}
-            >
-              <ArrowRight size={16} className="mr-2 text-muted-foreground" />
-              {message.heading}
-            </Button>
-          ))}
+          {/* Example messages removed */}
         </div>
       </div>
     </div>
