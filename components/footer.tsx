4eb14df72fbbd9c4b2a1330c39f26789302dc22f
aditya-gaharawar <164782232+aditya-gaharawar@users.noreply.github.com>
2024-04-25 10:01:46 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
       <div className="flex justify-end">
         {/* GitHub, Twitter, and Discord icons removed */}
       </div>
-      <p className="text-sm text-gray-500">WEBSPACEAI Research can make mistakes. Consider checking important information.</p>
+      <p className="text-sm text-gray-500">WEBSPACEAI Research can make mistakes.</p>
     </footer>
   )
 }
